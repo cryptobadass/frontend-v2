91c1324579c1b3904ded1b59898457633c1d1493
Lance lin <lance.lin@apcapitalinvestment.com>
2022-04-21 15:22:58 +0800

    fix: get fuji price

--- src/services/coingecko/coingecko.service.ts
+++ src/services/coingecko/coingecko.service.ts
@@ -9,7 +9,7 @@ export const getNativeAssetId = (chainId: string): string => {
     '42': 'ethereum',
     '137': 'matic-network',
     '42161': 'ethereum',
-    '43113': 'fuji',
+    '43113': 'ethereum',
     '43114': 'avalanche-2'
   };
   // avalanche-2
@@ -23,7 +23,7 @@ export const getPlatformId = (chainId: string): string => {
     '42': 'ethereum',
     '137': 'polygon-pos',
     '42161': 'arbitrum-one',
-    '43113': 'avalanche',
+    '43113': 'ethereum',
     '43114': 'avalanche'
   };
   // avalanche-2
